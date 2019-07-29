1b8616611325f08a205badb8407667428bbc94d1
alanbjohnston <alan.b.johnston@gmail.com>
2019-07-29 03:37:49 -0400

    enabled buffer size

--- make_wav.c
+++ make_wav.c
@@ -84,7 +84,7 @@ void write_wav(char * filename, unsigned long num_samples, short int * data, int
 
 #define S_RATE  (44100)
 define S_RATE  48000
-#define BUF_SIZE (S_RATE*10) /* 2 second buffer */
+define BUF_SIZE (S_RATE*10) /* 2 second buffer */
  
 //int buffer[BUF_SIZE];
 short int buffer[BUF_SIZE];
